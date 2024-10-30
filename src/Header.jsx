c800b7874d41ef6da79b1d29d29972380b1580ea
Arshveer Singh <arsh.veer0801@gmail.com>
2024-10-30 17:53:56 -0500

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+
+    return(
+        <>
+        <header>
+            <h1>Insert your MAL profile</h1>
+        </header>
+        <body>
+        <div class="outer-square">
+            <input type="text" class="center-input" placeholder="test" />
+            
+        </div>
+        </body>
+        </>
+    );
+}
+
+export default Header
